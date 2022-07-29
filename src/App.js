9cdc82457ba15445715f76741d400162e9fd8229
dangtuan08 <48054696+dangtuan08@users.noreply.github.com>
2022-07-29 23:06:35 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Box } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      APP
-    </div>
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
