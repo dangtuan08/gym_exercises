147aca3a65796891f86d528ec68768c7586eda42
Tuan <tuanou17@gmail.com>
2022-08-01 15:49:47 +0700

    Body Part Component done

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,10 +1,52 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useContext } from "react";
+import { Box, Typography } from "@mui/material";
 import BodyPart from "./BodyPart";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+
+import leftArrowIcon from "../assets/icons/left-arrow.png";
+import RightArrowIcon from "../assets/icons/right-arrow.png";
+
+
+const scrollVisibilityApiType =  VisibilityContext;
+
+function LeftArrow() {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()}>
+      <img src={leftArrowIcon} alt="left arrow" />;
+    </Typography>
+  );
+}
+
+function RightArrow() {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()}>
+      <img src={RightArrowIcon} alt="right arrow" />
+    </Typography>
+  );
+}
+
+// function onWheel( scrollVisibilityApiType, React.onWheel): void {
+//   const isThouchpad = Math.abs(ev.deltaX) !== 0 || Math.abs(ev.deltaY) < 15;
+
+//   if (isThouchpad) {
+//     ev.stopPropagation();
+//     return;
+//   }
+
+//   if (ev.deltaY < 0) {
+//     apiObj.scrollNext();
+//   } else if (ev.deltaY > 0) {
+//     apiObj.scrollPrev();
+//   }
+// }
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
-    <div>
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {data.map((item) => {
         return (
           <Box
@@ -21,7 +63,7 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
           </Box>
         );
       })}
-    </div>
+    </ScrollMenu>
   );
 };
 
